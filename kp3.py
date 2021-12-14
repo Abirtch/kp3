3e674678e68dd4cec1800f156e90ef659889379b
Abirtch <95928111+Abirtch@users.noreply.github.com>
2021-12-14 20:27:55 +0700

    Update kp3.py

--- kp3.py
+++ kp3.py
@@ -4,12 +4,12 @@ y = []
 z = []
 print('Введите координаты вектора x')
 for i in range(3):
-    x_var = int(input())
-    x.append(x_var)
+    x_k = int(input())
+    x.append(x_k)
 print('Введите координаты вектора y')
 for i in range(3):
-    y_var = int(input())
-    y.append(y_var)
+    y_k = int(input())
+    y.append(y_k)
 a = int(input('Введите скаляр a'))
 zx = x[0] * y[0] * a
 zy = x[1] * y[1] * a
